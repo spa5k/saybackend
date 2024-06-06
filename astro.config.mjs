ac299d5761b6f0df5898f0e452205fc762e1763e
Spark <79936503+spa5k@users.noreply.github.com>
2024-06-06 11:29:23 +0530

    chore: update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,9 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
-const LIVE_URL = "https://www.saybackend.com";
-// this is the astro command your npm script runs
-const isBuild = SCRIPT.includes("astro build");
-const BASE_URL = isBuild ? LIVE_URL : LOCALHOST_URL;
-
 // https://astro.build/config
 export default defineConfig({
-  site: BASE_URL,
+  site: "https://www.saybackend.com",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
