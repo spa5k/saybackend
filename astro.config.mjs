dc115d00117d7956b9af25ecefff3ddcef26c322
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-07 04:06:26 +0000

    chore: Add robots.txt file and update npm dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
-import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 import pagefind from "astro-pagefind";
+import { defineConfig } from "astro/config";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.saybackend.com",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  integrations: [tailwind(), sitemap(), mdx(), pagefind(), compress()],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
