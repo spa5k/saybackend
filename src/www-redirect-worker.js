d8fee22c0f29e4353d30bd2a84a8cbb1b62b55f5
spa5k <79936503+spa5k@users.noreply.github.com>
2026-06-19 17:11:53 +0530

    fix: split saybackend www redirect worker

---
+++ src/www-redirect-worker.js
@@ -0,0 +1,9 @@
+const canonicalOrigin = "https://saybackend.com";
+
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+
+    return Response.redirect(`${canonicalOrigin}${url.pathname}${url.search}`, 301);
+  },
+};
